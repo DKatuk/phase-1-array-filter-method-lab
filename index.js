8fc34f635ce415d4760e881ed00bd3b3fd01b381
Dilara Katuk <dilarakatuk@gmail.com>
2022-05-09 14:55:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+function findMatching(array, str){
+   return array.filter(function(element){
+            return element.toLowerCase() === str.toLowerCase();
+        });
+}
+
+function fuzzyMatch(array, str){
+    return array.filter( function(initial) {
+        return initial[0] === str[0];
+    })
+};
+
+function matchName(array, str){
+    return array.filter(function(query){
+        return query.name === str;
+    })
+}
+
